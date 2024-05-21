4b12e06f6637d709027f4d230865a1ec989ff834
Daniel Holmes <senditholmes@gmail.com>
2024-05-21 22:13:44 +0900

    add guess validation logic

--- wordle-clone/wordle.js
+++ wordle-clone/wordle.js
@@ -1,20 +1,51 @@
+// GLOBALS
+const currentGuessNumber = "1";
+
+// ELEMENTS
+const currentRow = document.querySelector(`#r${currentGuessNumber}`);
+currentRow.addEventListener("keyup", handleInput);
 const wordOfTheDayContainer = document.querySelector(
   ".word-of-the-day-container"
 );
 
-const guessButton = document.querySelector(".guess-button");
-guessButton.addEventListener("click", handleGuess);
+// HELPERS
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
 
-function handleGuess(event) {
-  let guess = [];
-  guess[0] = document.querySelector("#row1letter1").value;
-  guess[1] = document.querySelector("#row1letter2").value;
-  guess[2] = document.querySelector("#row1letter3").value;
-  guess[3] = document.querySelector("#row1letter4").value;
-  guess[4] = document.querySelector("#row1letter5").value;
+// EVENT LISTENER
+function handleInput(event) {
+  let guess = Array(5);
+  guess[0] = document.querySelector(`#r${currentGuessNumber}l1`).innerHTML;
+  guess[1] = document.querySelector(`#r${currentGuessNumber}l2`).innerHTML;
+  guess[2] = document.querySelector(`#r${currentGuessNumber}l3`).innerHTML;
+  guess[3] = document.querySelector(`#r${currentGuessNumber}l4`).innerHTML;
+  guess[4] = document.querySelector(`#r${currentGuessNumber}l5`).innerHTML;
+  const wordToValidate = guess.join("");
+  console.log(wordToValidate);
+  console.log(guess);
 
-  const guessAsString = guess.join("");
-  console.log(guessAsString);
+  if (!guess.includes("<br>")) {
+    const guessJSON = { word: wordToValidate };
+    handleValidate(guessJSON);
+  }
+}
+
+async function handleValidate(guessJSON) {
+  const response = await fetch("https://words.dev-apis.com/validate-word", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(guessJSON),
+  });
+  const guessConfirmation = await response.json();
+
+  if (guessConfirmation.validWord === true) {
+    console.log("VALID WORD + CORRECT LENGTH");
+  } else {
+    console.log("Word is not valid mate");
+  }
 }
 
 async function getWordOfTheDay() {
