ea6031435fffe54e296eadd1ae2a93479ce9fd18
Daniel Holmes <senditholmes@gmail.com>
2024-05-23 09:18:48 +0900

    finished focus logic on input elements

--- wordle-clone/wordle.js
+++ wordle-clone/wordle.js
@@ -1,12 +1,17 @@
 // GLOBALS
 const currentGuessNumber = "1";
+const max = 1;
+let wordOfTheDay;
+let puzzleNumber;
 
 // ELEMENTS
 const currentRow = document.querySelector(`#r${currentGuessNumber}`);
-currentRow.addEventListener("keyup", handleInput);
+currentRow.addEventListener("keydown", handleInput);
 const wordOfTheDayContainer = document.querySelector(
   ".word-of-the-day-container"
 );
+const firstBox = document.querySelector("#r1l1");
+firstBox.focus();
 
 // HELPERS
 function isLetter(letter) {
@@ -15,20 +20,53 @@ function isLetter(letter) {
 
 // EVENT LISTENER
 function handleInput(event) {
-  let guess = Array(5);
-  guess[0] = document.querySelector(`#r${currentGuessNumber}l1`).innerHTML;
-  guess[1] = document.querySelector(`#r${currentGuessNumber}l2`).innerHTML;
-  guess[2] = document.querySelector(`#r${currentGuessNumber}l3`).innerHTML;
-  guess[3] = document.querySelector(`#r${currentGuessNumber}l4`).innerHTML;
-  guess[4] = document.querySelector(`#r${currentGuessNumber}l5`).innerHTML;
-  const wordToValidate = guess.join("");
-  console.log(wordToValidate);
-  console.log(guess);
+  if (
+    isLetter(event.key) ||
+    event.key === "Backspace" ||
+    event.key === "Delete"
+  ) {
+    setTimeout(function () {
+      let guess = Array(5);
 
-  if (!guess.includes("<br>")) {
-    const guessJSON = { word: wordToValidate };
-    handleValidate(guessJSON);
+      for (let i = 0; i < guess.length; i++) {
+        guess[i] = document.querySelector(
+          `#r${currentGuessNumber}l${i + 1}`
+        ).value;
+      }
+      const wordToValidate = guess.join("");
+      console.log(wordToValidate);
+      console.log(guess);
+
+      if ((wordToValidate.length === 5) & !guess.includes("")) {
+        console.log("sending to validate");
+        const guessJSON = { word: wordToValidate };
+        handleValidate(guessJSON);
+      }
+    });
+
+    if (
+      (event.key === "Backspace") &
+      (event.target.previousElementSibling != null)
+    ) {
+      setTimeout(function () {
+        event.target.previousElementSibling.focus();
+      });
+    }
+  } else {
+    event.preventDefault();
   }
+
+  setTimeout(function () {
+    if (
+      (event.target.nextElementSibling != null) &
+      (event.key != "Backspace")
+    ) {
+      event.target.nextElementSibling.focus();
+    }
+  });
+
+  // if the current event input = 1
+  // AND previous keypress was NOT backspace, auto next focus
 }
 
 async function handleValidate(guessJSON) {
@@ -40,11 +78,18 @@ async function handleValidate(guessJSON) {
     body: JSON.stringify(guessJSON),
   });
   const guessConfirmation = await response.json();
+  let currentGuess = guessConfirmation.word;
 
   if (guessConfirmation.validWord === true) {
-    console.log("VALID WORD + CORRECT LENGTH");
+    compareWord(wordOfTheDay, currentGuess);
+  }
+}
+
+function compareWord(wordOfTheDay, currentGuess) {
+  if (wordOfTheDay === currentGuess) {
+    alert("YOU WON!");
   } else {
-    console.log("Word is not valid mate");
+    alert("WRONG GUESS");
   }
 }
 
@@ -52,16 +97,10 @@ async function getWordOfTheDay() {
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const data = await res.json();
 
-  const wordOfTheDay = data.word;
-  const puzzleNumber = data.puzzleNumber;
+  wordOfTheDay = data.word;
+  puzzleNumber = data.puzzleNumber;
 
   wordOfTheDayContainer.innerText = `Today's word: ${wordOfTheDay}. Puzzle Number: ${puzzleNumber}`;
 }
 
-// PROGRAM START
-
-function init() {
-  getWordOfTheDay();
-}
-
-init();
+getWordOfTheDay();
