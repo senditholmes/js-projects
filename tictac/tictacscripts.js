1f1c43b069ed9126c6fade4484fab107e7a2c192
Daniel Holmes <senditholmes@gmail.com>
2024-06-09 21:44:15 +0900

    add function for checking win conditions

--- tictac/tictacscripts.js
+++ tictac/tictacscripts.js
@@ -1,24 +1,58 @@
-let squares = document.querySelectorAll(".square");
-let playerOneturn = true;
-let playerTwoTurn = false;
+const squares = document.querySelectorAll(".square");
+let numberOfPlayedTurns = 0;
+const playerOne = {
+  name: document.querySelector("#p1-name").innerText,
+  symbol: "X",
+  currentTurn: true,
+};
+const playerTwo = {
+  name: document.querySelector("#p2-name").innerText,
+  symbol: "O",
+  currentTurn: false,
+};
+const players = [playerOne, playerTwo];
 
-squares.forEach((square) => {
-  // add event listener
-  square.addEventListener("click", function handleUserTurn(e) {
-    const clickedSquare = e.currentTarget;
-    // if square is not blank, return
-    if (clickedSquare.innerText != "") {
-      return;
-    }
+function handleUserTurn(e) {
+  const currentGuesses = new Array();
+  const clickedSquare = e.currentTarget;
+  // if square is not blank, return
+  if (clickedSquare.innerText != "") {
+    return;
+  }
 
-    if (playerOneturn) {
-      clickedSquare.innerText = "O";
-      previousGuess = "O";
-    } else if (playerTwoTurn) {
-      clickedSquare.innerText = "X";
-      previousGuess = "X";
-    }
-    playerOneturn = !playerOneturn;
-    playerTwoTurn = !playerTwoTurn;
+  // else enter correct symbol
+  if (playerOne.currentTurn === true) {
+    clickedSquare.innerText = playerOne.symbol;
+    previousGuess = playerOne.symbol;
+  } else if (playerTwo.currentTurn === true) {
+    clickedSquare.innerText = playerTwo.symbol;
+    previousGuess = playerTwo.symbol;
+  }
+  playerOne.currentTurn = !playerOne.currentTurn;
+  playerTwo.currentTurn = !playerTwo.currentTurn;
+  numberOfPlayedTurns++;
+
+  squares.forEach((square) => {
+    currentGuesses.push(square.innerText);
   });
-}); // END OF FIRST FOREACH
+
+  // is win possible?
+  if (numberOfPlayedTurns > 4) {
+    checkWin(currentGuesses);
+  }
+}
+
+function checkWin(currentGuesses, numberOfPlayedTurns) {
+  console.log(currentGuesses);
+
+  // loop top left to bottom right
+  for (let i = 0; i < currentGuesses.length; i++) {}
+}
+
+function init() {
+  squares.forEach((square) => {
+    square.addEventListener("click", handleUserTurn);
+  });
+}
+
+init();
